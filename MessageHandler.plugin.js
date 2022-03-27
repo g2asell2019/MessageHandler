4026fd642554eb32a6d2d55070144b713a37ed48
g2asell2019 <51025883+g2asell2019@users.noreply.github.com>
2022-03-27 17:28:33 +0700

    Update MessageHandler.plugin.js

--- MessageHandler.plugin.js
+++ MessageHandler.plugin.js
@@ -4,7 +4,7 @@
  * @name MessageHandler_Halola
  * @author Halola
  * @authorId 737323631117598811
- * @version 3.6.4
+ * @version 0.0.1
  * @description abc
  * @source https://github.com/g2asell2019
  * @updateUrl https://raw.githubusercontent.com/oSumAtrIX/BetterDiscordPlugins/master/
@@ -17,7 +17,7 @@
 		return "MessageHandler";
 	}
 	getVersion() {
-		return "3.6.4";
+		return "0.0.1";
 	}
 	getAuthor() {
 		return "Halola";
